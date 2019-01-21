cfbe6b040c1363944895071dc00a0ee5a1c64193
Yaz Saito <yasushi.saito@gmail.com>
2019-01-20 22:01:09 -0800

    Fix the noncgo code.

--- creadline/readline_nocgo.go
+++ creadline/readline_nocgo.go
@@ -5,17 +5,18 @@ package creadline
 import (
 	"bufio"
 	"os"
+	"strings"
 )
 
 var stdinReader *bufio.Reader
 
-func Readline(prompt string) string {
+func Readline(prompt string) (string, error) {
 	if stdinReader == nil {
 		stdinReader = bufio.NewReader(os.Stdin)
 	}
 	os.Stdout.Write([]byte(prompt))
-	text, _ := stdinReader.ReadString('\n')
-	return text
+	line, err := stdinReader.ReadString('\n')
+	return strings.TrimSpace(line), err
 }
 
 func AddHistory(str string)                                                        {}
@@ -31,5 +32,7 @@ func HistoryGetHistoryState() (h HistoryState)                                  
 func HistoryExpand(val string) (out string, ret int)                               { return val, 0 }
 func ReadInitFile(path string) error                                               { return nil }
 func SetAttemptedCompletionFunction(fn func(line string, start, end int) []string) { return }
+
+func GetScreenSize() (int, int) { return 0, 0 }
 
 func Init() {}
